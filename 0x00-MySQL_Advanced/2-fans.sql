cb17ebe05f5e931182ef75179c5ee9e11c095fbd
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-08 17:58:44 +0100

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
--- SQL script that ranks country origins of bands
-
-
+-- SQL script to rank country origins by the number of non-unique fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
