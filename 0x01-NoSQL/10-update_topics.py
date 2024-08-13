d0c169ca57a3652b81cbf45b114d298e7be0b62b
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-13 10:58:11 +0100

    ayo what was i doing

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""Change school topics"""
 from pymongo import MongoClient
 
 
 def update_topics(mongo_collection, name, topics):
-    name = "Holberton school"
-    topics = ['Sys admin', 'AI', 'Algorithm']
-    mongo_collection.update_Many(name, topics)
+    """Change school topics"""
+    mongo_collection.update_many({"name": name}, 
+                                 {"$set": {"topics": topics}})
