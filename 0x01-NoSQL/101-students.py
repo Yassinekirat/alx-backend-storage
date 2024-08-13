63ff491ead7307d27a5ce9eccb5ad2cd39248490
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-13 11:23:46 +0100

    task 14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+""" Top students"""
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    """ Top students"""
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
