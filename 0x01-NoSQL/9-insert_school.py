fc48b7a2abc76dd086ab3bfc86331afbd385379c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-13 10:44:52 +0100

    maybe this will fix

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@ from pymongo import MongoClient
 def insert_school(mongo_collection, **kwargs):
     """Insert a document in Python"""
     result = mongo_collection.insert_one(kwargs)
-    return str(result.inserted_id)
+    return result.inserted_id
