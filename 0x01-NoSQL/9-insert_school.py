592923a1f06f8073ce4c87d5ede84dc67e64c03f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-13 10:38:53 +0100

    idk

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Insert a document in Python"""
+from pymongo import MongoClient
 
 
+def insert_school(mongo_collection, **kwargs):
+    """Insert a document in Python"""
+    result = mongo_collection.insert_one(kwargs)
+    return str(result.inserted_id)
