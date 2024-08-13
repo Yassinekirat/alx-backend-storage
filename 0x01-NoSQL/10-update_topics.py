d04ef4c91d163b52d1f7035f20622155a2efbb4a
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-13 10:56:44 +0100

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    name = "Holberton school"
+    topics = ['Sys admin', 'AI', 'Algorithm']
+    mongo_collection.update_Many(name, topics)
