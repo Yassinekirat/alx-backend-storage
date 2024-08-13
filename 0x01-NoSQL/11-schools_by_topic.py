9f9421faf3b6bbecb7e151bfdab1cc01ac3930c1
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-13 11:03:11 +0100

    task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""returns the list of school having a specific topic"""
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    mongo_collection.find({"$set": {"topic": topic}})
