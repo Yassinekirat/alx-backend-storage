0e9a6de02deb132dc47d23eb67fd5a559ce9b712
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-14 10:31:54 +0100

    task 1 """ Writing strings to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+""" Writing strings to Redis"""
+import redis
+import uuid
+from typing import Union
 
 
+class Cache:
+    def __init__(self):
+        """starting constructor method python"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Writing strings to Redis"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
